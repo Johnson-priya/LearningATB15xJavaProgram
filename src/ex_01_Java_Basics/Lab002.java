4bd1a13bfcd0aed679715bddc20a32e7294d903a
Jpriya23 <jhnsnpriya@gmail.com>
2025-12-17 11:04:54 +0530

    2nd excercise commit

---
+++ src/ex_01_Java_Basics/Lab002.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basics;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int a= 9;
+        System.out.printf("%d X 1 = %d%n" , a, (a));
+        System.out.printf("%d X 2 = %d%n" , a, (a*2));
+        System.out.printf("%d X 3 = %d%n" , a, (a*3));
+        System.out.printf("%d X 4 = %d%n" , a, (a*4));
+        System.out.printf("%d X 5 = %d%n" , a, (a*5));
+        System.out.printf("%d X 6 = %d%n" , a, (a*6));
+        System.out.printf("%d X 7 = %d%n" , a, (a*7));
+        System.out.printf("%d X 8 = %d%n" , a, (a*8));
+        System.out.printf("%d X 9 = %d%n" , a, (a*9));
+        System.out.printf("%d X 10 = %d%n" , a, (a*10));
+
+
+    }
+}
