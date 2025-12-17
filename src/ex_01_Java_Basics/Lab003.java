e90163b8cf354ba5fdb661ac3d17c671becf78cd
Jpriya23 <jhnsnpriya@gmail.com>
2025-12-17 12:36:59 +0530

    4th excercise commit update

--- src/ex_01_Java_Basics/Lab003.java
+++ src/ex_01_Java_Basics/Lab003.java
@@ -1,11 +1,18 @@
 package ex_01_Java_Basics;
 
+import java.util.Scanner;
+
 public class Lab003 {
     public static void main(String[] args) {
-        int a= 100;
-        int b= 345;
+
+        //Taking input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("first number:");
+        int a= sc.nextInt();
+        System.out.println("Second Number:");
+        int b= sc.nextInt();
         //System.out.println("sum of %d and %d: " + (a+b));
-        System.out.printf("The sum of %d and %d is: %d%n" , a, b, (a+b));
+        System.out.printf("The sum of %d and %d is: %d%n" ,a,b,(a+b));
 
     }
 }
