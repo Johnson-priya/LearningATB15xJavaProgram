5c53ed53d4bfe0b6de63156a4db0e8818d6630cf
Jpriya23 <jhnsnpriya@gmail.com>
2025-12-17 11:24:37 +0530

    3rd excercise commit

---
+++ src/ex_01_Java_Basics/BasicDataTypes.java
@@ -0,0 +1,24 @@
+package ex_01_Java_Basics;
+
+public class BasicDataTypes {
+    public static void main(String[] args) {
+        int salary= 45000;
+        byte age= 27;
+        short year= 2037;
+        float price= 99.99f;
+        long population= 14000000000L;
+        double pi= 3.178990093;
+        boolean isJavaFun= true;
+        System.out.println("Salary:" + salary);
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Price: " + price);
+        System.out.println("Price: " + population);
+        System.out.println("pi: " + pi);
+        System.out.println("Java is fun? " + isJavaFun);
+
+
+
+
+    }
+}
