3faaa391781b9e8bf8b2205a4d2e065fa9e7fdf9
Jpriya23 <jhnsnpriya@gmail.com>
2025-12-17 11:34:37 +0530

    4th excercise commit

---
+++ src/ex_01_Java_Basics/Lab003.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int a= 100;
+        int b= 345;
+        //System.out.println("sum of %d and %d: " + (a+b));
+        System.out.printf("The sum of %d and %d is: %d%n" , a, b, (a+b));
+
+    }
+}
