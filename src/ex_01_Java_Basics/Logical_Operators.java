eb6b1eaeee96f0e67467ce4c348fa45c892b8964
Jpriya23 <jhnsnpriya@gmail.com>
2026-01-10 18:22:46 +0530

    Lab04_To_Three_Max commit

---
+++ src/ex_01_Java_Basics/Logical_Operators.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Logical_Operators {
+    public static void main(String[] args) {
+        boolean n= true;
+        System.out.println(!n);
+
+        boolean b= false;
+        System.out.println(!!b);
+
+    }
+}
---
+++ src/ex_01_Java_Basics/concat.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class concat {
+    public static void main(String[] args) {
+        String first_name = "Johnson";
+        String last_name = "Priya";
+        int a= 10;
+        int b= 30;
+        System.out.println(first_name + last_name + a + b);
+        System.out.println(a + b + first_name + last_name);
+        System.out.println(first_name + " " + last_name + " " + (a+b));
+
+    }
+}
